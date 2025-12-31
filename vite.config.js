106fbfd898ae64eb2586d9e2dc52cf1381e9ed50
yousufg <yousuforgbd@gmail.com>
2025-12-31 15:36:17 +0600

    U

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
+  plugins: [react()],
+  base: '/my-project/'  // Must exactly match your repository name
+});
